34d91e73b22375fc82825f51b05f13880242a85e
Suhaas142 <35166597+Suhaas142@users.noreply.github.com>
2022-11-21 12:54:56 -0600

    Add Owned NFT Changes

--- nft-app/API/Home.py
+++ nft-app/API/Home.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pandas.io import json
 import config as cg
 from flask import jsonify
-
+import sys
 
 class Home:
 
@@ -12,6 +12,17 @@ class Home:
     def getnftDataForHome(self,trader_id):
         conn = cg.connect_to_mySQL()
         query = f"SELECT * FROM test.nft WHERE owner_id <> {trader_id}"
+        self.df1 = pd.read_sql(query,conn)
+        if not self.df1.empty:
+            json_nft_data = self.df1.to_json(orient = "index")
+            print(type(json_nft_data),file = sys.stderr)
+            
+            parsed_json = json.loads(json_nft_data)
+            return json.dumps(parsed_json)
+
+    def getnftDataForTrader(self,trader_id):
+        conn = cg.connect_to_mySQL()
+        query = f"SELECT * FROM test.nft WHERE owner_id = {trader_id}"
         self.df1 = pd.read_sql(query,conn)
         if not self.df1.empty:
             json_nft_data = self.df1.to_json(orient = "index")
